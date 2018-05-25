81f73775e32e3c1ad535cad2140653838dae5ab9
ZeroNerodaHero <billyz404meter@gmail.com>
2018-05-25 14:18:33 -0700

    Update PBot.java

--- PBot.java
+++ PBot.java
@@ -1,29 +1,12 @@
 package bsn;
 
 import robocode.*;
-
-
 import java.awt.*;
 import java.awt.geom.*;
 import java.util.*;
 import java.util.Random;
- 
-
 import robocode.util.*;
 
-
-
-/**
- * A modular bot adhering to the RoboPart Interface.
- * 
- * @author TODO Your Name
- * @version TODO Date
- * 
- * @author Period - TODO Your Period
- * @author Assignment - PartsBot
- * 
- * @author Sources - TODO list collaborators
- */
 public class PBot extends AdvancedRobot
 {
     private AdvancedEnemyBot enemy = new AdvancedEnemyBot();
@@ -38,11 +21,6 @@ public class PBot extends AdvancedRobot
     
     static double enemyEnergy;
 
-
-    /**
-     * something
-     */
-
     public void run()
     {
         parts[RADAR] = new Radar();
@@ -52,20 +30,12 @@ public class PBot extends AdvancedRobot
         enemy.reset();
         
         enemyEnergy = 100;
-        
-        // initialize each part
         for ( int i = 0; i < parts.length; i++ )
         {
-            // behold, the magic of polymorphism
             parts[i].init();
         }
-
-        // iterate through each part, moving them as we go
         for ( int i = 0; true; i = ( i + 1 ) % parts.length )
         {
-            
-            
-            
             parts[i].move();
             
             if ( i == 0 )
@@ -74,14 +44,7 @@ public class PBot extends AdvancedRobot
             }
         }
     }
-
-
-    /**
-     * something
-     * 
-     * @param e
-     *            something
-     */
+    
     public void onScannedRobot( ScannedRobotEvent e )
     {
         double absBearing=e.getBearingRadians()+getHeadingRadians();
@@ -94,13 +57,6 @@ public class PBot extends AdvancedRobot
         
     }
 
-
-    /**
-     * something
-     * 
-     * @param e
-     *            something
-     */
     public void onRobotDeath( RobotDeathEvent e )
     {
         
@@ -108,7 +64,6 @@ public class PBot extends AdvancedRobot
         if ( radar.wasTracking( e ) ) {
             enemy.reset();
         }
-            
     }
 
     // ... put normalizeBearing and absoluteBearing methods here
@@ -125,55 +80,15 @@ public class PBot extends AdvancedRobot
         return angle;
     }
     
-    
-    // ... declare the RobotPart interface and classes that implement it here
-    // They will be _inner_ classes.
-    /**
-     * 
-     * @return interface something
-     */
     public interface RobotPart
     {
-        /**
-         * something
-         * TODO Write your method description here.
-         */
         public void init();
-        
-        /**
-         * something
-         * TODO Write your method description here.
-         */
         public void move();
     }
 
-
-    
-
-    /**
-     * 
-     * TODO Write a one-sentence summary of your class here. TODO Follow it with
-     * additional details about its purpose, what abstraction it represents, and
-     * how to use it.
-     *
-     * @author shuang248
-     * @version May 11, 2018
-     * @author Period: TODO
-     * @author Assignment: Robo05PartsBot
-     *
-     * @author Sources: TODO
-     */
     public class Radar implements RobotPart
     {
-        /**
-         * something
-         */
         int radarDirection;
-
-
-        /**
-         * something
-         */
         public void init()
         {
             setAdjustRadarForGunTurn( true );
@@ -183,95 +98,46 @@ public class PBot extends AdvancedRobot
 
         }
 
-
-        /**
-         * something
-         */
         public void move()
         {
             
             if ( enemy.none() )
             {
-                // look around
                 setTurnRadarRight( Double.POSITIVE_INFINITY );
                 
             }
             else
             {
-                // keep him inside a cone
                 double turn = normalizeBearing(enemy.getBearing() - getRadarHeading()
                    + getHeading());
                 
                 turn += 10* radarDirection;
                 setTurnRadarRight( turn );
                 radarDirection *= -1;
-                
-                
             }
-            
         }
 
-
-        /**
-         * something
-         * 
-         * @return something
-         * @param e something
-         */
         public boolean shouldTrack( ScannedRobotEvent e )
         {
-            // track if we have no enemy, the one we found is significantly
-            // closer, or we scanned the one we've been tracking.
             return ( enemy.none() || e.getDistance() < enemy.getDistance() - 70
                 || e.getName().equals( enemy.getName() ) );
         }
 
-
-        /**
-         * something
-         * 
-         * @return soemthing
-         * @param e something
-         */
         public boolean wasTracking( RobotDeathEvent e )
         {
             return e.getName().equals( enemy.getName() );
         }
     }
 
-
-    /**
-     * 
-     * TODO Write a one-sentence summary of your class here. TODO Follow it with
-     * additional details about its purpose, what abstraction it represents, and
-     * how to use it.
-     *
-     * @author shuang248
-     * @version May 11, 2018
-     * @author Period: TODO
-     * @author Assignment: Robo05PartsBot
-     *
-     * @author Sources: TODO
-     */
     public class Gun implements RobotPart
     {
-        /**
-         * something
-         */
         public void init()
         {
             setAdjustGunForRobotTurn(true);
-            
-            //setTurnGunRightRadians(- getHeading());
         }
 
-
-        /**
-         * something
-         */
         public void move()
         {
-         // don't shoot if I've got no enemy
             if ( enemy.none() ) {
                 return;
             }
@@ -287,30 +153,12 @@ public class PBot extends AdvancedRobot
             // calculate gun turn to predicted x,y location
             double futureX = enemy.getFutureX( time );
             double futureY = enemy.getFutureY( time );
-            //double absDeg = absoluteBearing(getX(), getY(), enemy.getX(),enemy.getY());
-            // non-predictive firing can be done like this:
-            // double absDeg = absoluteBearing(getX(), getY(), enemy.getX(),enemy.getY());
-            /**
-            if (absDeg > 247) {
-                setFire(1);
-            }
-            */
-            // turn the gun to the predicted x,y location
-            setTurnGunRight( normalizeBearing( absoluteBearing - getGunHeading()) );
-
-            // if the gun is cool and we're pointed in the right direction, shoot!
             
+            setTurnGunRight( normalizeBearing( absoluteBearing - getGunHeading()) );
             if ( getGunHeat() == 0 && Math.abs( getGunTurnRemaining() ) < 10 )
             {
                 setFire( firePower );
             }
-            
-            /**
-            double energyChange=(enemyEnergy-(enemyEnergy=enemy.getEnergy()));
-            if(energyChange<=3&&energyChange>=0.1){
-                setFire(firePower);
-            }
-            */
         }
         
         
